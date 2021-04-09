5f6c5e393640a8ea14eb7e3dd0d13b3f96e73c8e
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-04-09 11:38:58 +0300

    Function for login screen

--- app/src/main/java/fi/oamk/androidapp/SecondFragment.kt
+++ app/src/main/java/fi/oamk/androidapp/SecondFragment.kt
@@ -23,10 +23,10 @@ class SecondFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
     }
 
     override fun onCreateView(
