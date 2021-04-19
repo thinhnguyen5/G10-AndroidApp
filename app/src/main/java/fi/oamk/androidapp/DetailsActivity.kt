8e8460e1cd00808edfb5e382fb43403ef3791fbb
ngocdu <ngocdu171@gmail.com>
2021-04-19 09:46:18 +0300

    show image and price into Details page

--- app/src/main/java/fi/oamk/androidapp/DetailsActivity.kt
+++ app/src/main/java/fi/oamk/androidapp/DetailsActivity.kt
@@ -2,11 +2,22 @@ package fi.oamk.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 class DetailsActivity : AppCompatActivity() {
 
+    private lateinit var database: DatabaseReference
+
+    private lateinit var ivImage: ImageView
     private lateinit var tvKey: TextView
+    private lateinit var tvName: TextView
+    private lateinit var tvprice: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +26,30 @@ class DetailsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        ivImage = findViewById(R.id.image)
         tvKey = findViewById(R.id.key)
+        tvName = findViewById(R.id.name)
+        tvprice = findViewById(R.id.price)
 
         val key = intent.getStringExtra("key").toString()
         tvKey.text = key
+
+        database = Firebase.database.reference
+
+        database.child("items").child(key).get().addOnSuccessListener {
+            if (it.value != null) {
+                val item = it.value as HashMap<String, Any>
+                val image = item.get("image")
+                val name = item.get("name")
+                val price = item.get("price")
+
+                Picasso.get().load(image.toString()).into(ivImage)
+                tvName.text = name.toString()
+                tvprice.text = price.toString()
+            }
+        }.addOnFailureListener{
+            //
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
