1f8f0071868d14ce14bfada664aeff53af2e1ba0
ngocdu <ngocdu171@gmail.com>
2021-04-22 11:27:15 +0300

    improvement code

--- app/src/main/java/fi/oamk/androidapp/DetailsActivity.kt
+++ app/src/main/java/fi/oamk/androidapp/DetailsActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -14,6 +15,7 @@ import com.squareup.picasso.Picasso
 import kotlin.reflect.typeOf
 
 class DetailsActivity : AppCompatActivity() {
+//class DetailsActivity : Fragment() {
 
     private lateinit var database: DatabaseReference
 
@@ -46,6 +48,9 @@ class DetailsActivity : AppCompatActivity() {
 
         database = Firebase.database.reference
 
+        //button Add To Cart
+        btnAddCart = findViewById(R.id.btn_addCart)
+
         //set status for item
         var tam: String = ""
 
@@ -67,45 +72,73 @@ class DetailsActivity : AppCompatActivity() {
                 tvName.text = name.toString()
                 tvprice.text = price.toString()
                 tam = status.toString()
+                Log.e("tam", "$tam")
+                if (tam == "Out of Stock") {
+                    btnAddCart.isEnabled = false
+                    Toast.makeText(this, "DISABLE", Toast.LENGTH_LONG).show()
+                    Log.e("button", "disable")
+                }
+                else {
+                    btnAddCart.setOnClickListener{
+//                        Toast.makeText(this, "$tam", Toast.LENGTH_LONG).show()
+//                        tester1()
+
+//                        startActivity<MainActivity>("name" to name)
+//                        requireActivity().getSharedPreferences()
+
+                        //pass value but have to redirect to another
+//                        val intent = Intent(this, CartActivity::class.java).apply {
+//                            putExtra("name", name.toString())
+//                        }
+//                        startActivity(intent)
+                    }
+                }
             }
         }.addOnFailureListener{
             //
         }
 
 //        tam1 = etQuantity * price
+//        Log.d("taasdzjhgcjhaghdkjgszjkhdgasm", "$tam")
+//        Toast.makeText(this, "$tam", Toast.LENGTH_LONG).show()
 
-        btnAddCart = findViewById(R.id.btn_addCart)
-        btnAddCart.setOnClickListener() {
-//            Log.d("kakak","$tam")
-            if (tam == "Out of Stock") {
-                AlertDialog.Builder(this)
-                        .setMessage("$tam")
-                        .setPositiveButton("OK", object : DialogInterface.OnClickListener {
-                            override fun onClick(p0: DialogInterface?, p1: Int) {
-//                                val intent = Intent(this, MainActivity::class.java)
-//                                startActivity(intent)
-//                                val intent = Intent(this,RegisterActivity::class.java)
-//                                startActivity(intent)
-//                                startActivity(Intent(this, MainActivity::class.java))
-                            }
-                        })
-                        .create()
-                        .show()
-            }
-            else {
+
+
+//        btnAddCart.setOnClickListener() {
+////            Log.d("kakak","$tam")
+//            if (tam == "Out of Stock") {
 //                AlertDialog.Builder(this)
-//                        .setMessage("Added Successfully!")
+//                        .setMessage("$tam")
 //                        .setPositiveButton("OK", object : DialogInterface.OnClickListener {
 //                            override fun onClick(p0: DialogInterface?, p1: Int) {
-////                                TODO("Not yet implemented")
+////                                val intent = Intent(this, MainActivity::class.java)
+////                                startActivity(intent)
+////                                val intent = Intent(this,RegisterActivity::class.java)
+////                                startActivity(intent)
+////                                startActivity(Intent(this, MainActivity::class.java))
 //                            }
 //                        })
 //                        .create()
 //                        .show()
-            }
-//            Toast.makeText(this, "$tam", Toast.LENGTH_LONG).show()
-        }
+//            }
+//            else {
+////                AlertDialog.Builder(this)
+////                        .setMessage("Added Successfully!")
+////                        .setPositiveButton("OK", object : DialogInterface.OnClickListener {
+////                            override fun onClick(p0: DialogInterface?, p1: Int) {
+//////                                TODO("Not yet implemented")
+////                            }
+////                        })
+////                        .create()
+////                        .show()
+//            }
+////            Toast.makeText(this, "$tam", Toast.LENGTH_LONG).show()
+//        }
     }
+
+//    private fun tester1() {
+//        Toast.makeText(this, "$tam", Toast.LENGTH_LONG).show()
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
